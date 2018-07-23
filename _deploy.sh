20598a1219f610a12ca9183f63432002ed3f3d6f
zachbogart <zlbogart@gmail.com>
2018-07-23 15:14:18 -0400

    update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "email@provider.com"
-git config --global user.name "Insert Username Here"
+git config --global user.email "zb2223@columbia.edu" # add your github email
+git config --global user.name "Zach Bogart" # change to your username
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
